89f227e27c3e252e0eb1afc22454e56518e2347f
Shazzana <shazzana.shahar@gmail.com>
2023-02-04 09:14:46 +0800

    edited typo

--- src/Components/IngredientSearchForm.js
+++ src/Components/IngredientSearchForm.js
@@ -6,7 +6,7 @@ import './IngredientSearchForm.css';
 function IngredientSearchForm() {
     return (
         <div className='Form'>
-            <h1 className='Form-text'>Is this making you itch?</h1>
+            <h1 className='Form-text'>Is it making you itch?</h1>
             <p className='Form-para'>Some food ingredients could trigger histamine release, causing you to itch.
                 Search an ingredient to find out more.
             </p>
