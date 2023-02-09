5cf718413edc30dd256c56f6a8055e4133944e3e
Shazzana <shazzana.shahar@gmail.com>
2023-02-09 11:53:31 +0800

    edited search result page header

--- src/Components/SearchResultDisplay.js
+++ src/Components/SearchResultDisplay.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function SearchResultDisplay() {
     return (
-        <h1>Search Results</h1>
+        <div>
+            <br/>
+            <h1>Search Results</h1>
+        </div>
+        
     )
 }
 
