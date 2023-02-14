8a41d1744c9591342f81e6b0a815721450010ff5
Shazzana <shazzana.shahar@gmail.com>
2023-02-14 21:20:38 +0800

    search result tables created

--- src/Components/SearchResultDisplay.js
+++ src/Components/SearchResultDisplay.js
@@ -1,13 +1,133 @@
-import React from 'react';
+import React from "react";
+import Table from "react-bootstrap/Table";
 
 function SearchResultDisplay() {
-    return (
-        <div>
-            <br/>
-            <h1>Search Results</h1>
-        </div>
-        
-    )
+  return (
+    <div>
+      <br />
+      <h1>Search Results</h1>
+      <Table striped bordered>
+        <thead>
+          <tr>
+            <th>Ingredient</th>
+            <th>Remarks</th>
+            <th>Histamine</th>
+            <th>Other Amines</th>
+            <th>Liberator</th>
+            <th>Blocker</th>
+            <th>Compatibility</th>
+            <th>Add to list</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Avocado</td>
+            <td>-</td>
+            <td>
+              <i class="fa-solid fa-check"></i>
+            </td>
+            <td>
+              <i class="fa-solid fa-check"></i>
+            </td>
+            <td>
+              <i class="fa-solid fa-check"></i>
+            </td>
+            <td>
+              <i class="fa-solid fa-check"></i>
+            </td>
+            <td>3</td>
+            <td>
+              <i class="fa-solid fa-circle-plus"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>Ginger</td>
+            <td>Anti-inflammatory</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>0</td>
+            <td>
+              <i class="fa-solid fa-circle-plus"></i>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      <Table striped bordered>
+        <thead>
+            <tr>
+                <td colSpan={4}>Mechanisms affecting histamine metabolism</td>
+                <td colSpan={4}>Histamine Compatibility Scale</td>
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td colSpan={2}>Histamine</td>
+            <td colSpan={2}>High histamine content</td>
+            <td colSpan={2}>0</td>
+            <td colSpan={2}>
+              Well tolerated, no symptoms expected at usual intake
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Other Amines</td>
+            <td colSpan={2}>Other biogenic amines</td>
+            <td colSpan={2}>1</td>
+            <td colSpan={2}>
+            Moderately compatible, minor symptoms, occasional 
+            consumption of small quantities is often tolerated 
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Liberators</td>
+            <td colSpan={2}>Histamine liberators</td>
+            <td colSpan={2}>2</td>
+            <td colSpan={2}>
+            Incompatible, significant symptoms at usual intake
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Blockers</td>
+            <td colSpan={2}>Inhibitors of diamine oxidase and other histamine degrading enzymes</td>
+            <td colSpan={2}>3</td>
+            <td colSpan={2}>
+            Very poorly tolerated, severe symptoms
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2}></td>
+            <td colSpan={2}></td>
+            <td colSpan={2}>-</td>
+            <td colSpan={2}>
+            No general statement possible
+            </td>
+          </tr>
+          <tr>
+          <td colSpan={2}></td>
+            <td colSpan={2}></td>
+            <td colSpan={2}>?</td>
+            <td colSpan={2}>
+            Insufficient or contradictory information
+            </td>
+          </tr>
+        </tbody>
+        </Table>
+    </div>
+  );
 }
 
 export default SearchResultDisplay;
+
+// 1: Moderately compatible, minor symptoms, occasional 
+// consumption of small quantities is often tolerated 
+// 2: Incompatible, significant symptoms at usual intake
+// 3: Very poorly tolerated, severe symptoms
+// -: No general statement possible
+// ?: Insufficient or contradictory information
+
+// A: Other biogenic amines
+// L : Liberators of mast cell mediators
+// (=histamine liberators)
+// B: Blocker (=inhibitors) of diamine oxidase
+// or of other histamine degrading enzymes
